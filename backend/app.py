2aebbe889a5c080be1eae818672d37c4e9ab8378
Alvaro-SP <socop2412@gmail.com>
2023-04-10 23:10:44 -0600

    [ADD] the JWT implementing for endpoints
    JWT implementing for endpoints to check all autorizations of users

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,8 @@ from src.usuarios.GetCategories import getCategories
 from src.usuarios.Empresas_category import empresaCategoria
 from src.usuarios.Products_empresa import productsEmpresa
 import datetime
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                            unset_jwt_cookies, jwt_required, JWTManager
 
 #* Flask config
 app = Flask(__name__)
@@ -16,13 +18,15 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
+app.config["JWT_SECRET_KEY"] = "ojala-me-muera"
+jwt = JWTManager(app)
 
 #! ------------------- CONNECT WITH DATABASE:-------------------
 conn  = mysql.connector.connect(
     user='root',
     password='alvaro',
     host='34.28.45.29',
-    database='dbayd',
+    database='dbproyectoayd',
     port='3306'
 )
 #* Crea el cursor para ejecutar las consultas
@@ -32,27 +36,36 @@ mycursor = conn.cursor()
 
 #*  ************************ LOGIN ************************
 @app.route('/login', methods=['POST'])
-def ver_catalogo():
-    global conn
-    response = login(conn, request)
+def loginzzzz():
+    global conn, jwt
+    response = login(conn, request, jwt)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"res": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
 #*  ********************** REGISTRO ************************
 @app.route('/register', methods=['POST'])
-def ver_catalogo():
+def registerzzz():
     global conn
     response = registro(conn, request)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
 #*  ************************  USUARIOS ************************
 @app.route('/getcategories', methods=['GET'])
-def getCategories():
+@jwt_required()
+def get_Categories():
     global conn
     response = getCategories(conn)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 @app.route('/empresas-category', methods=['POST'])
+@jwt_required()
 def empresacategory():
     global conn
     response = empresaCategoria(conn, request)
@@ -60,7 +73,8 @@ def empresacategory():
     return response
 
 @app.route('/products-empresa', methods=['POST'])
-def empresacategory():
+@jwt_required()
+def empresacategorias():
     global conn
     response = productsEmpresa(conn, request)
     response.headers.add('Access-Control-Allow-Origin', '*')
